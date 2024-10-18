c628aa61099b8673c9b0f23249aea5d1cc55ed0d
Dubabbi <thdms3160@naver.com>
2024-10-18 15:53:54 +0900

    Design: Navbar 미디어 쿼리 적용

--- myapp/src/components/Header/Header.jsx
+++ myapp/src/components/Header/Header.jsx
@@ -1,32 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './header.css';
-import logoImg from '../../assets/icon/logo.svg'; // 로고 이미지 경로
+import logoImg from '../../assets/icon/logo.svg'; 
+import menuIcon from '../../assets/icon/menu.svg'; 
+import closeIcon from '../../assets/icon/close.svg'; 
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // 토글 상태 관리
   const navigate = useNavigate();
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen); // 토글 상태 변경
+  };
+
   return (
     <>
-    <header className="header">
-      <div className="logo" onClick={() => navigate('/')}>
-        <img src={logoImg} alt="Logo" className="logo-image" /> {/* 로고 이미지 추가 */}
-        csowne
-      </div>
-      <nav className="navbar">
-        <a href="/features">Features</a>
-        <a href="/docs">Docs</a>
-        <a href="/blog">Blog</a>
-        <a href="/subject">Subject</a>
-      </nav>
-      <div className="cta">
-        <button className="signup-btn">Contact</button>
-      </div>
-      <div className="header-hr-container"> 
+      <header className="header">
+        <div className="logo" onClick={() => navigate('/')}>
+          <img src={logoImg} alt="Logo" className="logo-image" /> {/* 로고 이미지 추가 */}
+          csowne
+        </div>
+
+        <nav className={`navbar ${isMenuOpen ? 'open' : ''}`}>
+          <a href="/features">Features</a>
+          <a href="/docs">Docs</a>
+          <a href="/blog">Blog</a>
+          <a href="/subject">Subject</a>
+        </nav>
+
+        <div className="cta">
+          <button className="signup-btn">Contact</button>
+        </div>
+
+        <div className="menu-toggle" onClick={toggleMenu}>
+          <img src={isMenuOpen ? closeIcon : menuIcon} alt="Menu" className="menu-icon" />
+        </div>
+        <div className="header-hr-container"> 
           <hr className="navbar-divider" />
         </div>
-    </header>
-        </>
+      </header>
+    </>
   );
-}
+};
 
 export default Header;
